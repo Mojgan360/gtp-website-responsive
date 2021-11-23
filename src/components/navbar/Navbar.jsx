6831196996b6fb276d51ffc5d20dbc7600b3da68
Mojgan360 <mojgan360@gmail.com>
2021-11-23 19:07:53 +0100

    clean-up

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from 'react'
 import './navbar.css'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
-import { FaBars } from 'react-icons/fa'
 
 import logo from '../../assets/logo.svg'
 import { links } from '../../utils/data'
 
 const Navbar = () => {
-  // const [showLinks, setShowLinks] = useState(false)
+  const [showLinks, setShowLinks] = useState(false)
 
-  // const toggleLinks = () => {
-  //   setShowLinks(!showLinks)
-  // }
+  const toggleLinks = () => {
+    setShowLinks(!showLinks)
+  }
   return (
     <nav className='gtp3__navbar'>
       <div className='gtp3__navbar-center'>
         {/* nav-header */}
         <div className='gtp3__navbar-header'>
           <img src={logo} alt='logo' />
-          <div className='gtp3__navbar-header_toggle_menu'>
-            <RiMenu3Line />
-          </div>
+          <button
+            className='gtp3__navbar-header_toggle_menu'
+            onClick={() => toggleLinks()}
+          >
+            {showLinks ? <RiCloseLine /> : <RiMenu3Line />}
+          </button>
         </div>
         {/* nav-container */}
         <div className='gtp3__navbar-containers'>
