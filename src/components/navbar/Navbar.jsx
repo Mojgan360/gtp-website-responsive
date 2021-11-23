c9422f7a5e5a6124721cb83cc6efbd8217a9b669
Mojgan360 <mojgan360@gmail.com>
2021-11-23 18:36:22 +0100

    Added navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import { FaBars } from 'react-icons/fa'
+
+import logo from '../../assets/logo.svg'
+import { links } from '../../utils/data'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  // const [showLinks, setShowLinks] = useState(false)
+
+  // const toggleLinks = () => {
+  //   setShowLinks(!showLinks)
+  // }
+  return (
+    <nav className='gtp3__navbar'>
+      <div className='gtp3__navbar-center'>
+        {/* nav-header */}
+        <div className='gtp3__navbar-header'>
+          <img src={logo} alt='logo' />
+          <div className='gtp3__navbar-header_toggle_menu'>
+            <RiMenu3Line />
+          </div>
+        </div>
+        {/* nav-container */}
+        <div className='gtp3__navbar-containers'>
+          <ul className='links'>
+            {links.map((item) => {
+              return (
+                <li>
+                  <a href={item.url}>{item.text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <div className='gtp3__navbar-signin'>
+          <p>Sign in</p>
+          <button>Sign Up</button>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
---
+++ src/utils/data.js
@@ -0,0 +1,7 @@
+export const links = [
+  { id: 1, url: '/', text: 'Home' },
+  { id: 1, url: '/wgpt3', text: 'What is GPT3?' },
+  { id: 1, url: '/possibility', text: 'Open AI' },
+  { id: 1, url: '/features', text: 'Case Studies' },
+  { id: 1, url: '/blog', text: 'Library' },
+]
