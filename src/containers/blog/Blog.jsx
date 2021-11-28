d479349446b747915d513a66a45357fa24450299
Mojgan360 <mojgan360@gmail.com>
2021-11-28 20:51:12 +0100

    Added Blog

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Article } from '../../components/index'
 
 const Blog = () => {
-  return <div>Blog</div>
+  return (
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-header'>
+        <h1 className='gradient__text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore natus
+          optio facere esse distinctio accusantium minus recusandae{' '}
+        </h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container__GroupA'>
+          <Article />
+        </div>
+        <div className='gpt3__blog-container__GroupB'>
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Blog
